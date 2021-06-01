52d0ef16825311704be1e132354bbbc0439a2dd9
Seth Duncan <sethduncan@gmail.com>
2021-05-31 21:19:51 -0500

    store some game data in local files

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const Player = require('./lib/Player');
 // generate map for this session
 let activeMap = new Map(process.env.SYSTEMS_TO_GENERATE);
 let mapSystems = activeMap.getSystemIds();
+
+// initialize player
 let player = new Player(mapSystems[Math.floor(Math.random() * mapSystems.length)]);
 
-console.log(player);
+console.log(activeMap.getSystemObject());
