e82623fde44bd3013c8d1644de1d22a7d50ba31b
Seth Duncan <sethduncan@gmail.com>
2021-05-31 23:17:08 -0500

    build a bunch of command line stuff so the game, you know, runs

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 require('dotenv').config();
 const Galaxy = require('./lib/Galaxy');
 const Player = require('./lib/Player');
+const Game = require('./lib/Game');
 
 // generate map for this session
 let activeGalaxy = new Galaxy();
-let mapSystems = activeGalaxy.getSystemIds();
 
 // initialize player
-let player = new Player(mapSystems[Math.floor(Math.random() * mapSystems.length)]);
+let player = new Player(activeGalaxy);
+
+// generate the game instance
+let gameInstance = {
+    galaxy: activeGalaxy,
+    player: player
+};
+
+let activeGame = new Game(gameInstance);
